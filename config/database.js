640c7387be7dfae3c17cfcfa3fec3ae2018318d5
Nishtha Jaiswal <nishthajaiswal@gmail.com>
2024-06-11 14:17:26 +0530

    first commit,created all the models that has been used in this project

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose");
+require("dotenv").config;
+
+exports.connect=()=>{
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser:true, 
+        useUnifiedTopology:true,
+
+    })
+    .then(()=> console.log("DB connection successfully"))
+    .catch((error)=> {
+        console.log("DB connection failed");
+        console.error(error);
+        process.exist(1);
+    })
+}
