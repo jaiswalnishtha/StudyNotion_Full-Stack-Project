e1b900617591d4fd475f8e9824d2a458ac05c52d
jaiswalnishtha <nishthajaiswal222@gmail.com>
2024-06-21 17:03:48 +0530

    working dashbaord page created

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import {Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"
 import Navbar from "./components/common/Navbar"
 import OpenRoute from "./components/core/Auth/OpenRoute"
+
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
-import Catalog from "./pages/Catalog";
-import { BrowserRouter } from "react-router-dom";
 import ForgotPassword from "./pages/ForgotPassword";
+import UpdatePassword from "./pages/UpdatePassword";
+import VerifyEmail from "./pages/VerifyEmail";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./components/core/Dashboard/MyProfile";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/core/Auth/PrivateRoute";
+import Error from "./pages/Error"
+
 function App() {
   return (
    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
-   <Navbar/>
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path = "catalog/:catalogName" element={<Catalog/>}/>
       <Route
           path="signup"
           element={
@@ -27,10 +34,11 @@ function App() {
           path="login"
           element={
             <OpenRoute>
-              <Login/>
+              <Login />
             </OpenRoute>
           }
         />
+
     <Route
           path="forgot-password"
           element={
@@ -38,9 +46,54 @@ function App() {
               <ForgotPassword />
             </OpenRoute>
           }
+        />  
+
+      <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />  
+
+    <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />  
+
+    <Route
+          path="about"
+          element={
+            <OpenRoute>
+              <About />
+            </OpenRoute>
+          }
         />
-    </Routes>
+    <Route path="/contact" element={<Contact />} />
+
+    <Route 
+      element={
+        <PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>
+      }
+    >
+      <Route path="dashboard/my-profile" element={<MyProfile />} />
+      {/* <Route path="dashboard/settings" element={<Setting />} /> */}
+
+    </Route>
+
     
+
+    <Route path="*" element={<Error />} />
+
+
+    </Routes>
 
    </div>
   );
